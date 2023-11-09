853a722939a82921c15ef4894466960c91665300
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-09 09:17:24 -0600

    Add generalized operate function
    Uses switch statement to check case for each operator

--- calc.js
+++ calc.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +16,23 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(op, a, b) {
+    switch (op) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+        default:
+            break;
+    }
 }

9d90237afd4403313b142b3c8ba139021b02ec65
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-09 11:58:16 -0600

    Add event listeners and display logic

--- calc.js
+++ calc.js
@@ -1,7 +1,3 @@
-let num1;
-let num2;
-let operator;
-
 function add(a, b) {
     return a + b;
 }
@@ -15,6 +11,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) return "Are you crazy?";
     return a / b;
 }
 
@@ -28,7 +25,62 @@ function operate(op, a, b) {
             return multiply(a, b);
         case "/":
             return divide(a, b);
+        case "":
+            return a;
         default:
-            return "Invalid operator";
+            return `Invalid operator: "${op}"`;
     }
 }
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+const numBtns = document.querySelectorAll(".number");
+const opBtns = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear");
+const equalsBtn = document.querySelector(".equals");
+const display = document.querySelector(".display");
+
+function clearDisplay() {
+    display.textContent = "";
+    num1 = "";
+    num2 = "";
+    operator = "";
+}
+
+function calculate() {
+    let result = (num1 !== "") ? operate(operator, +num1, +num2) : num2;
+
+    if (isNaN(result)) {
+        num1 = "";
+    } else {
+        result = Math.round(result * 1000) / 1000;
+        num1 = result;
+    }
+
+    console.log(`${num1} ${operator} ${num2} = ${result}`)
+    display.textContent = result;
+    num2 = "";
+    operator = "";
+
+    return result;
+}
+
+numBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        operator === "" ? num1 += button.textContent : num2 += button.textContent;
+        operator === "" ? display.textContent = num1 : display.textContent = num2;
+        if (display.textContent.startsWith("0")) display.textContent = display.textContent.slice(1);
+    });
+});
+
+opBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        if (operator !== "") calculate();
+        operator = button.textContent;
+    });
+});
+
+clearBtn.addEventListener("click", clearDisplay);
+equalsBtn.addEventListener("click", calculate);
